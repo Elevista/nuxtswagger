6c7d2672aa8bfb92b8ea63e419af284cef359d4a
Elevista <sunnyholic@sunnyholic.com>
2021-10-10 02:08:34 +0900

    space after colon

--- src/TemplateCommon.ts
+++ src/TemplateCommon.ts
@@ -4,8 +4,8 @@ import { camelCase } from './utils'
 import * as v2 from './schema/v2/Spec'
 import * as v3 from './schema/v3/Spec'
 import { LoDashStatic } from 'lodash'
-type ParameterIn = v2.ParameterIn| v3.ParameterIn | '$body' | '$config'
-interface Parameter { type: string, required: boolean, name: string, valName:string, pos: ParameterIn }
+type ParameterIn = v2.ParameterIn | v3.ParameterIn | '$body' | '$config'
+interface Parameter { type: string, required: boolean, name: string, valName: string, pos: ParameterIn }
 type Response = v2.Response | v3.Response
 type TypeDefs = v2.Types | v2.ParameterTypes | v3.Types | v3.ParameterTypes | {} | Boolean
 type Spec = v2.Spec | v3.Spec
@@ -15,12 +15,12 @@ type Schemas = v2.Definitions | v3.Schemas
 enum MethodTypes {get = 'get', post = 'post', put = 'put', patch = 'patch', delete = 'delete', head = 'head', options = 'options'}
 export type TemplateOptions = Options & { relTypePath: string }
 
-const _:LoDashStatic = require('lodash')
+const _: LoDashStatic = require('lodash')
 const typeMatch = ['integer', 'long'].map(x => `type ${x} = number`).join('\n')
-const entriesCompare = <T>([a]:[string, T], [b]:[string, T]) => a.localeCompare(b)
+const entriesCompare = <T>([a]: [string, T], [b]: [string, T]) => a.localeCompare(b)
 const exists = <TValue>(value: TValue | null | undefined): value is TValue => !!value
 const noInspect = '/* eslint-disable */\n// noinspection ES6UnusedImports,JSUnusedLocalSymbols\n'
-const escapeRegExp = (string:string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+const escapeRegExp = (string: string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 const replaceAll = (string: string, searchValue: string, replaceValue: string) => string.replace(new RegExp(escapeRegExp(searchValue), 'g'), replaceValue)
 const genericVar = (i: number) => {
   const arr = ['T', 'U', 'V']
@@ -28,12 +28,12 @@ const genericVar = (i: number) => {
 }
 
 export abstract class TemplateCommon {
-  protected abstract spec:Spec
-  protected readonly relTypePath:string
-  protected readonly basePath:string
-  protected readonly inject:string
-  protected readonly skipHeader:boolean
-  protected constructor (spec:Spec, { pluginName, basePath, inject, skipHeader, relTypePath }:TemplateOptions) {
+  protected abstract spec: Spec
+  protected readonly relTypePath: string
+  protected readonly basePath: string
+  protected readonly inject: string
+  protected readonly skipHeader: boolean
+  protected constructor (spec: Spec, { pluginName, basePath, inject, skipHeader, relTypePath }: TemplateOptions) {
     this.relTypePath = relTypePath
     this.basePath = basePath
     this.skipHeader = skipHeader
@@ -41,8 +41,8 @@ export abstract class TemplateCommon {
     this.fixRefDeep(spec)
   }
 
-  protected abstract get schemas():Schemas
-  protected abstract getResponseType(response:Response):string
+  protected abstract get schemas(): Schemas
+  protected abstract getResponseType(response: Response): string
 
   protected fixTypeName = (name: string) => name
     .replace(/^#\/(components\/schemas|definitions)\//, '')
@@ -52,7 +52,7 @@ export abstract class TemplateCommon {
     .replace(/.+/, camelCase)
     .replace(/[ ]+$/g, '')
 
-  protected comment (comment?:string|number|boolean|object, onlyText = false) {
+  protected comment (comment?: string|number|boolean|object, onlyText = false) {
     if (comment === undefined) { return '' }
     if (comment === Object(comment)) comment = JSON.stringify(comment)
     const string = comment.toString().trim()
@@ -74,12 +74,12 @@ export abstract class TemplateCommon {
     return comment && this.comment(comment, onlyText)
   }
 
-  protected typeDeep (typeObj:TypeDefs, maxIndent = -1, noComment = false):string {
+  protected typeDeep (typeObj: TypeDefs, maxIndent = -1, noComment = false): string {
     if (typeof typeObj === 'boolean') return 'any'
     const canComment = maxIndent >= 0 && !noComment
     const indentProps = maxIndent > 0
     const comment = canComment ? this.makeComment(typeObj) : ''
-    const typeDeep = (typeObj:TypeDefs) :string => {
+    const typeDeep = (typeObj: TypeDefs): string => {
       if ('schema' in typeObj) return typeDeep(typeObj.schema)
       if ('$ref' in typeObj) return (typeObj.$ref in this.schemas) ? typeObj.$ref : 'any'
       if ('enum' in typeObj) {
@@ -89,7 +89,7 @@ export abstract class TemplateCommon {
       if (typeObj.type === 'array') return `Array<${typeDeep(typeObj.items)}>`
       if (typeObj.type === 'object') {
         const { properties, additionalProperties, required = [] } = typeObj
-        const entries:[string, TypeDefs][] = Object.entries(properties || {})
+        const entries: [string, TypeDefs][] = Object.entries(properties || {})
         if (additionalProperties) entries.push(['[key in any]', additionalProperties])
         if (!entries.length) return 'any'
         const items = entries.map(([name, value]) => {
@@ -106,7 +106,7 @@ export abstract class TemplateCommon {
   }
 
   protected toArgs (parameters: Parameter[]) {
-    const classify = (params:Parameter[] = []) => {
+    const classify = (params: Parameter[] = []) => {
       const [names, all, required, optional] = [[], [], [], []] as string[][]
       params.forEach((p) => {
         names.push(p.valName)
@@ -116,7 +116,7 @@ export abstract class TemplateCommon {
       })
       return { names, types: { all, required, optional, ordered: [...required, ...optional] } }
     }
-    const toArgs = (parameters:Parameter[]) => {
+    const toArgs = (parameters: Parameter[]) => {
       const { path = [], body = [], $config, ...others } = _.groupBy(parameters, x => x.pos)
       if ((parameters.length - (path.length + body.length)) < 5) return classify(parameters).types.ordered
       const pathBody = classify([...path, ...body]).types.all // should be no optional type in path and body
@@ -127,14 +127,14 @@ export abstract class TemplateCommon {
     return toArgs(parameters).join(', ')
   }
 
-  protected fixKeys<T extends object> (o:T):T {
-    const ret:any = {}
+  protected fixKeys<T extends object> (o: T): T {
+    const ret: any = {}
     Object.entries(o).forEach(([key, value]) => { ret[this.fixTypeName(key)] = value })
     return ret
   }
 
-  protected fixRefDeep (spec:Spec) {
-    const deep = (o:any) => {
+  protected fixRefDeep (spec: Spec) {
+    const deep = (o: any) => {
       if (!(o instanceof Object)) return
       if ('$ref' in o) o.$ref = this.fixTypeName(o.$ref)
       else if (o instanceof Array) o.forEach(deep)
@@ -152,7 +152,7 @@ export abstract class TemplateCommon {
     const types = Object.entries(this.schemas).sort(entriesCompare)
       .map(([rawName, type]) => {
         const [, name = rawName, genericString = ''] = rawName.match(/(.+?)<(.+)>/) || []
-        const genericReplacer = (str:string) => {
+        const genericReplacer = (str: string) => {
           const generics = genericString
             .replace(/<.+>/, x => x.replace(/,/g, '\x00'))
             .split(',')
@@ -177,7 +177,7 @@ export abstract class TemplateCommon {
 
   public plugin () {
     const { paths } = this.spec
-    const propTree:{[paths:string]:string} = {}
+    const propTree: {[paths: string]: string} = {}
     const base = this.basePath
     Object.entries(paths).sort(entriesCompare).forEach(([path, methods]) => {
       const keyPath = (path.startsWith(base + '/') ? path.replace(base, '') : path)
@@ -228,7 +228,7 @@ export abstract class TemplateCommon {
     params.push(...arrOf.queries)
     if (body) { params.push(body) }
     params.push(...arrOf.headers)
-    const $config:Parameter = {
+    const $config: Parameter = {
       name: '$config', valName: '$config', pos: '$config', type: 'AxiosRequestConfig', required: false
     }
     params.push($config)
