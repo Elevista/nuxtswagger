feb959c9773d1a4a4123453c283c0f92d570c841
Elevista <sunnyholic@sunnyholic.com>
2021-10-26 17:22:05 +0900

    chalk, package-lock

--- src/fetchSpec.ts
+++ src/fetchSpec.ts
@@ -1,7 +1,7 @@
-import * as c from 'chalk'
 import { fetch } from 'cross-fetch'
 import { Spec as v2 } from './schema/v2/Spec'
 import { Spec as v3 } from './schema/v3/Spec'
+const c = require('chalk')
 const fs = require('fs')
 const p = require('path')
 export default async function (path: string): Promise<v2 | v3> {
